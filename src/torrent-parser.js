bd3f360c596f94fcb0c1328cb21cd78b8ee7bf06
Leonco1 <leonid20021@gmail.com>
2024-01-29 21:22:18 +0100

    implemented size function

--- src/torrent-parser.js
+++ src/torrent-parser.js
@@ -3,14 +3,20 @@
 import * as fs from "fs"
 import bencode  from "bencode";
 import * as crypto from "crypto";
+import  * as bignum from 'bigint-buffer'
 
 export  function  open(filepath)
 {
 return bencode.decode(fs.readFileSync(filepath));
 }
 //torrents koristat SHA1 hashing function
-export  function  size(torrent)
 {
 const info=bencode.encode(torrent.info)
     return crypto.createHash("sha1").update(info).digest()
+}
+
+export function size(torrent)
+{
+    const size=torrent.info.files?torrent.info.files.map(file=>file.length).reduce((a,b)=>a+b):torrent.info.length
+    return bignum.toBufferBE(size,8)
 }
