9a9f706c1657e2051db0a51c60bd4db2bbd88e7c
Leonco1 <leonid20021@gmail.com>
2024-02-24 16:16:38 +0100

    add non-handshake message handlers

--- src/download.js
+++ src/download.js
@@ -5,22 +5,25 @@ import * as message from './message.js'
 
 export  function CreateTCPConnection(torrent)
 {
+    const requested=[];
     tracker.getPeers(torrent,peers=>
     {
-        peers.forEach(download)
+        peers.forEach(peer=>download(peer,torrent,requested))
     })
 }
 
-function download(peer) {
+
+function download(peer,torrent,requested) {
 
     const socket=new net.Socket
     socket.on("error",console.log)
     socket.connect(peer.port, peer.ip, function () {
         socket.write(Buffer.from('hello world'))
     })
-    onWholeMsg(socket,msg=>msgHandler(msg,socket))
+    const queue=[]
+    onWholeMsg(socket,msg=>msgHandler(msg,socket,requested,queue))
 }
-function msgHandler(msg,socket)
+function msgHandler(msg,socket,requested,queue)
 {
     if(isHandshake(msg))
     {
@@ -31,40 +34,61 @@ function msgHandler(msg,socket)
         const m=message.parse(msg)
         if(m.id===0) chokeHandler();
         if(m.id===1)unchokeHandler();
-        if(m.id===4)haveHandler(m.payload)
+        if(m.id===4)haveHandler(m.payload,socket,requested,queue)
         if(m.id===5)bitfieldHandler(m.payload)
-        if(m.id===7)pieceHandler(m.payload)
+        if(m.id===7)pieceHandler(m.payload,socket,requested,queue)
     }
 }
 function chokeHandler() {  }
 
 function unchokeHandler() {  }
 
-function haveHandler(payload) {  }
+function haveHandler(payload,socket,requested,queue) {
+    const pieceIndex = payload.readUInt32BE(0);
+    queue.push()
+    if(queue.length===1)
+    {
+        requestPiece(socket,requested,queue);
+    }
+    if (!requested[pieceIndex]) {
+        socket.write(message.buildRequest());
+    }
+    requested[pieceIndex] = true;
+}
 
 function bitfieldHandler(payload) {  }
 
-function pieceHandler(payload) {  }
+function pieceHandler(payload,socket,requested,queue) {
+    queue.shift()
+    requestPiece(socket, requested, queue);
 
+}
+function requestPiece(socket, requested, queue) {
+    if (requested[queue[0]]) {
+        queue.shift();
+    } else {
+        // complex arguments
+        socket.write(message.buildRequest(pieceIndex));
+    }
+}
 
-function onWholeMsg(socket,callback)
-{
-    let savedBuf=Buffer.alloc(0)
-    let handshake=true
-    socket.on('data',recvBuf=>{
+    function onWholeMsg(socket, callback) {
+        let savedBuf = Buffer.alloc(0)
+        let handshake = true
+        socket.on('data', recvBuf => {
 
-        const msgLen=()=>handshake? savedBuf.readUInt8(0)+49 :savedBuf.readInt32BE(0)+4
-        savedBuf = Buffer.concat([savedBuf, recvBuf]);
+            const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4
+            savedBuf = Buffer.concat([savedBuf, recvBuf]);
 
-        while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
-            callback(savedBuf.slice(0, msgLen()));
-            savedBuf = savedBuf.slice(msgLen());
-            handshake = false;
+            while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+                callback(savedBuf.slice(0, msgLen()));
+                savedBuf = savedBuf.slice(msgLen());
+                handshake = false;
             }
-    })
-}
-function isHandshake(msg)
-{
-    return msg.length===msg.readUInt8(0)+49&&
-        msg.toString('utf8',1)==="BitTorrent protocol"
-}
+        })
+    }
+
+    function isHandshake(msg) {
+        return msg.length === msg.readUInt8(0) + 49 &&
+            msg.toString('utf8', 1) === "BitTorrent protocol"
+    }
