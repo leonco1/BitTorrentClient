ab3abc28e67357be85a0d75ba5d15403b0b06376
Leonco1 <leonid20021@gmail.com>
2024-02-24 16:01:00 +0100

    Move parse function from download to message

--- src/message.js
+++ src/message.js
@@ -48,7 +48,7 @@ export  function KeepAlive()
 export  function buildChoke()
 {
     const buf=Buffer.alloc(5)
-    //lenght
+    //length
     buf.writeUInt32BE(1,0)
     //id
     buf.writeUInt8(0,4)
@@ -147,4 +147,28 @@ export  function buildPort(payload)
     buf.writeUInt8(9,4)
     buf.writeInt16BE(payload,5)
     return buf
+}
+
+export function parse(msg)
+{
+    const id=msg.length>4?msg.readInt8(4):null;
+    let payload=msg.length>5?msg.slice(5):null
+    if(id===6||id===7||id===8)
+    {
+        const rest=payload.slice(8)
+        payload=
+            {
+                index:payload.readInt32BE(0),
+                begin:payload.readInt32BE(4)
+            }
+        payload[id===7?'block':'length']=rest;
+
+    }
+    return {
+        size : msg.readInt32BE(0),
+        id : id,
+        payload : payload
+    }
+
+
 }
